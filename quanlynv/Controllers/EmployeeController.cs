56ac43213b3cba0cec8cb2256853eef6359d9a91
Lam12050504 <nguyenhuuduylinh23@gmail.com>
2021-07-12 14:44:08 +0700

    Add project files.

---
+++ quanlynv/Controllers/EmployeeController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using quanlynv.Models;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+namespace quanlynv.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private qlnvContext _context;
+        public EmployeeController(qlnvContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Employees);
+        }
+        public IActionResult Create()
+        {
+
+            return View(new Employee());
+        }
+        [HttpPost]
+        public IActionResult Create([FromForm] Employee model)
+        {
+            model.Created = DateTime.Now;
+            _context.Employees.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Detail(int id)
+        {
+            var employee = _context.Employees.Where(o => o.Id == id).FirstOrDefault();
+
+            return View(employee);
+        }
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.Where(o => o.Id == id).FirstOrDefault();
+
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, [FromForm] Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(model);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.Where(o => o.Id == id).FirstOrDefault();
+
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Delete(string btn, int id)
+        {
+            var employee = _context.Employees.Where(o => o.Id == id).FirstOrDefault();
+            if (btn == "Delete")
+            {
+
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        public IActionResult EditPicture(int id)
+        {
+            ViewBag.ImageUrl = null;
+            var empPict = this._context.EmployeePictures.Where(a => a.EmpId == id).FirstOrDefault();
+            if (empPict == null)
+                empPict = new EmployeePicture { EmpId = id };
+            else
+            {
+                string imgString = Convert.ToBase64String(empPict.ImageData);
+                string imageDataURL = string.Format("data:{0};base64,{1}", empPict.ImageType, imgString);
+
+                ViewBag.ImageUrl = imageDataURL;
+            }
+
+            return View(empPict);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditPicture([FromForm] EmployeePicture model, List<IFormFile> files)
+        {
+            if (ModelState.IsValid)
+            {
+                if (files.Count > 0)
+                {
+                    var formFile = files[0];
+
+                    MemoryStream ms = new();
+                    await formFile.CopyToAsync(ms);
+
+                    model.ImageData = ms.ToArray();
+                    model.ImageType = formFile.ContentType;
+                }
+                model.Created = DateTime.Now;
+
+                var empPict = this._context.EmployeePictures.Where(a => a.EmpId == model.EmpId).FirstOrDefault();
+                if (empPict == null)
+                {
+                    this._context.EmployeePictures.Add(model);
+                    this._context.SaveChanges();
+                }
+                else if (empPict != null && ModelState.IsValid) 
+                {
+                    this._context.EmployeePictures.Update(model);
+                    this._context.SaveChanges(model);
+                }
+
+                return RedirectToAction("ImageDetails", new { id = model.EmpId });
+            }
+            return View(model);
+        }
+        public IActionResult ImageDetails(int id)
+        {
+            ViewBag.ImageUrl = null;
+            var empPict = this._context.EmployeePictures.Where(a => a.EmpId == id).FirstOrDefault();
+            if (empPict != null)
+            {
+                string imgString = Convert.ToBase64String(empPict.ImageData);
+                string imageDataURL = string.Format("data:{0};base64,{1}", empPict.ImageType, imgString);
+
+                ViewBag.ImageUrl = imageDataURL;
+            }
+
+            var emp = this._context.Employees.Where(a => a.Id == id).FirstOrDefault();
+
+            return View(emp);
+        }
+    }
+   
+}
---
+++ quanlynv/Models/qlnvContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace quanlynv.Models
+{
+    public partial class qlnvContext : DbContext
+    {
+        public qlnvContext()
+        {
+        }
+
+        public qlnvContext(DbContextOptions<qlnvContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<EmployeePicture> EmployeePictures { get; set; }
+        public IEnumerable<object> EmployeePicture { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+ //           if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+  //              optionsBuilder.UseSqlServer("Server=DESKTOP-M9BS8LT\\SQLEXPRESS;Database=qlnv;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Created).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<EmployeePicture>(entity =>
+            {
+                entity.HasKey(e => e.EmpId);
+
+                entity.ToTable("EmployeePicture");
+
+                entity.Property(e => e.EmpId).ValueGeneratedNever();
+
+                entity.Property(e => e.Created).HasColumnType("datetime");
+
+                entity.Property(e => e.ImageData).IsRequired();
+
+                entity.Property(e => e.ImageInfo)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ImageType)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Emp)
+                    .WithOne(p => p.EmployeePicture)
+                    .HasForeignKey<EmployeePicture>(d => d.EmpId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_EmployeePicture_Employee");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        internal void SaveChanges(EmployeePicture model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
